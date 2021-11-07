c663bc17021441f132b16d5f8b670265126ad122
Tanzilha <tanzilahameed0@gmail.com>
2021-11-07 12:02:35 +0500

    first commit

---
+++ frontend/src/components/viewProductDescription.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ViewProductDescription() {
+    return (
+        <div>
+            product Description
+        </div>
+    )
+}
+
+export default ViewProductDescription
